660f150fa0d50c920c35b113b09b318a534ddebb
Gonzalo <INFORMATICA@DESKTOP-I8NP0CC>
2022-09-21 13:20:37 +0200

    BDA

--- src/aplicacion/Main.java
+++ src/aplicacion/Main.java
@@ -9,6 +9,12 @@ package aplicacion;
  *
  * @author somor
  */
-public class Main {
+public class Main { 
     
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
 }
